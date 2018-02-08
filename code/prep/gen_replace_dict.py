b2928522f25796683f9961806412a0444414fc72
JanzenLiu <chengtsung_liu@yahoo.com>
2018-02-08 23:33:53 +0800

    add codes to generate dictionary for special character replacement

--- code/prep/gen_replace_dict.py
+++ code/prep/gen_replace_dict.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json  # to dump python dict
 
 
-# --- load manually checked replacement schemes ---
+# --- load manually checked replacement schemes for equivalent brand names ---
 folder = './raw/'
 fname = 'brand_dist_1_v1_janzen.csv'
 df_d1_v1 = pd.read_csv(os.path.join(folder, fname))
@@ -21,7 +21,7 @@ print("num diff between two replace scheme:", len(diff_idx))
 # --- get replaceable pairs agreed by both schemes ---
 df_same = df_d1_v1.iloc[same_idx]
 df_p1 = df_same[df_same['replaceable'] == 1]
-dict_p1 = {row['brand_B']: row['brand_A'] for i, row in df_p1.iterrows()}
+dict_p1 = {row['brand_B']: row['brand_A'] for i, row in df_p1.iterrows()}  # generate dict
 print("num tuples in v1 replace dict:", len(dict_p1))
 
 
@@ -58,7 +58,7 @@ df_with_brand(df_train, 'Camilla')  # only one is left here because I did the in
 p2_idx = [68, 86, 87, 95, 106, 156, 267, 277, 366, 378]
 special_brand_list = ["Athelete", "MATRIX", "Elements", "Curve"]  # mislabelled brands found here
 df_p2 = df_d1_v1.iloc[p2_idx]
-dict_p2 = {row['brand_B']:row['brand_A'] for _, row in df_p2.iterrows()}
+dict_p2 = {row['brand_B']: row['brand_A'] for _, row in df_p2.iterrows()}  # generate dict
 
 
 # --- save part 2 replacement dictionary ---
@@ -74,3 +74,19 @@ with open(os.path.join(folder, fname), 'w') as f:
     for val in special_brand_list:
         f.write(val + '\n')
 
+
+# --- load manually checked replacement schemes for special characters ---
+folder = './raw/'
+fname = 'char_v1.csv'
+df_char_v1 = pd.read_csv(os.path.join(folder, fname))
+df_char_v1['to_replace_with'] = df_char_v1['to_replace_with'].map(
+    lambda x: x if len(x) == 1 else " {} ".format(x.strip())  # if the string used to replace is a word, add spaces
+)
+dict_char = {row['character']:row['to_replace_with'] for _, row in df_char_v1.iterrows()}  # generate dict
+
+
+# --- save replacement dictionary ---
+folder = './checked'
+fname = 'char_v1.dict'
+with open(os.path.join(folder, fname), 'w') as f:
+    json.dump(dict_char, f, indent=2)
