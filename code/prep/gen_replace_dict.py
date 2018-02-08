2ea52bcd7cd2684fd86c37270a108e2d5015e8a4
JanzenLiu <chengtsung_liu@yahoo.com>
2018-02-09 00:48:09 +0800

    add codes to extract lists of replaceable, unreplaceable and to-be-determined brands(A-K)

--- code/prep/gen_replace_dict.py
+++ code/prep/gen_replace_dict.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import os
 import pandas as pd
+import numpy as np
 import json  # to dump python dict
 
 
@@ -43,6 +44,8 @@ print(df_d1_v2.iloc[diff_idx])
 folder = '../../data/raw/'
 fname = 'train.tsv'
 df_train = pd.read_table(os.path.join(folder, fname))
+fname = 'test.tsv'
+df_test = pd.read_table(os.path.join(folder, fname))
 
 
 # --- helper function to find subset of DataFrame with certain brand ---
@@ -52,6 +55,7 @@ def df_with_brand(df, bname):
 
 # --- view brands ---
 df_with_brand(df_train, 'Camilla')  # only one is left here because I did the inspection in Jupyter Notebook
+df_with_brand(df_test, 'Camilla')  # only one is left here because I did the inspection in Jupyter Notebook
 
 
 # --- confirmed replacable pairs ---
@@ -82,7 +86,7 @@ df_char_v1 = pd.read_csv(os.path.join(folder, fname))
 df_char_v1['to_replace_with'] = df_char_v1['to_replace_with'].map(
     lambda x: x if len(x) == 1 else " {} ".format(x.strip())  # if the string used to replace is a word, add spaces
 )
-dict_char = {row['character']:row['to_replace_with'] for _, row in df_char_v1.iterrows()}  # generate dict
+dict_char = {row['character']: row['to_replace_with'] for _, row in df_char_v1.iterrows()}  # generate dict
 
 
 # --- save replacement dictionary ---
@@ -90,3 +94,51 @@ folder = './checked'
 fname = 'char_v1.dict'
 with open(os.path.join(folder, fname), 'w') as f:
     json.dump(dict_char, f, indent=2)
+
+
+# --- load manually checked fillable brand_name(A-K) from name ---
+folder = './raw/'
+fname = 'brand_in_name_A_K.xlsx'
+ws = pd.read_excel(os.path.join(folder, fname),
+                   sheet_name="results", usecols=["index", "name", "replace"],
+                   dtype={"index": np.int32, "name": str, "replace": np.int8})  # ws for worksheet
+ws['name'] = ws['name'].map(lambda x: x.replace('.test.tsv', '').replace('@@slash@@', '/'))  # correct error and decode
+
+
+# --- get replaceable, not replaceable and to-be-determined brands ---
+bin_no_repl = {}  # to store brands; bin for "brand in name", repl for replaceable
+bin_repl = {}
+bin_empty = {}
+bin_no_repl["A_K"] = ws[ws["replace"] == 0]["name"].tolist()
+bin_repl["A_K"] = ws[ws["replace"] == 1]["name"].tolist()
+bin_empty["A_K"] = ws[ws["replace"] == 2]["name"].tolist()
+
+
+# ===== helper class =====
+class FileSaver:
+    def __init__(self, folder):
+        self.folder = folder
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    def save_list(self, lst, fname):
+        assert isinstance(lst, list), "\"lst\" must be an instance of list"
+        with open(os.path.join(self.folder, fname + ".lst"), 'w') as f:
+            for v in lst:
+                f.write(str(v) + '\n')
+
+    def save_dict(self, dic, fname, **kw):
+        assert isinstance(dic, dict), "\"dic\" must be an instance of dict"
+        kw.setdefault("indent", 2)
+        with open(os.path.join(folder, fname + ".dict"), 'w') as f:
+            json.dump(dic, f, **kw)
+
+
+fs = FileSaver('./checked/')
+fs.save_list(bin_no_repl["A_K"], "brand_in_name_norepl_A_K")
+fs.save_list(bin_repl["A_K"], "brand_in_name_repl_A_K")
+fs.save_list(bin_empty["A_K"], "brand_in_name_empty_A_K")
+
+
+special_brand_list2 = []
+special_brand_list2.append("% Pure")
