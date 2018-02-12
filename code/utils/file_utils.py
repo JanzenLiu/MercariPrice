2bd37b1d569464db74bdf86132356215f862e2fb
JanzenLiu <chengtsung_liu@yahoo.com>
2018-02-12 12:31:09 +0800

    add FileSaver class in file_utils

---
+++ code/utils/file_utils.py
@@ -0,0 +1,43 @@
+import os
+import json
+import pandas as pd
+import pickle
+
+
+class FileSaver:
+    def __init__(self, folder):
+        self.folder = folder
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+    def _get_path(self, fname, ext):
+        return os.path.join(self.folder, "{}.{}".format(fname, ext))
+
+    def save_list(self, lst, fname):
+        assert isinstance(lst, list), "\"lst\" must be an instance of list"  # TODO(Janzen): to verify
+        path = self._get_path(fname, "lst")
+        with open(path, 'w') as f:
+            for v in lst:
+                f.write(str(v) + '\n')
+
+    def save_dict(self, dic, fname, **kw):
+        assert isinstance(dic, dict), "\"dic\" must be an instance of dict"
+        kw.setdefault("indent", 2)
+        path = self._get_path(fname, "dict")
+        with open(path, 'w') as f:
+            json.dump(dic, f, **kw)
+
+    def save_csv(self, df, fname, **kw):
+        assert isinstance(df, pd.DataFrame) or isinstance(df, pd.Series)  # TODO(Janzen): to verify
+        path = self._get_path(fname, "csv")
+        df.to_csv(path, **kw)
+
+    def save_tsv(self, df, fname, **kw):
+        assert isinstance(df, pd.DataFrame)
+        kw.pop("sep", None)  # pop item with key "sep" out
+        path = self._get_path(fname, "tsv")
+        df.to_csv(path, sep="\t", **kw)
+
+    def save_pickle(self, obj, fname, **kw):
+        path = self._get_path(fname, "pkl")
+        pickle.dump(obj, path, **kw)
